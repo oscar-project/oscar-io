5d2ed20a84181e7bfa1b7c80b4d448ebbc119e8b
jabadji <julien.abadji@inria.fr>
2023-01-18 16:51:23 +0100

    feat(v3-io): begin integrating v3 io

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,6 @@
 pub mod common;
 pub mod error;
 pub mod lang;
-pub mod oscar_doc;
+// pub mod oscar_doc;
+
+pub mod v3;
---
+++ src/v3/mod.rs
@@ -0,0 +1,4 @@
+mod types;
+
+pub use types::document::Document;
+pub use types::document::Metadata;
